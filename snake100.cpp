ca19bffd9373ff06c831bc378973ace2cd68efd8
Hajime Tamura <hajime0316@s.okayama-u.ac.jp>
2019-06-12 07:39:58 +0900

    Uncomment delay in initialize mode

--- snake100.cpp
+++ snake100.cpp
@@ -287,7 +287,7 @@ void loop() {
       Dxl.goalPosition( CommandParameters[2*ui], CommandParameters[2*ui +1]);
       Dxl.ledOn( CommandParameters[2*ui], color);
 
-      // delay(3);
+      delay(3);
     }
   }
   else { // モード初期化不要の場合
